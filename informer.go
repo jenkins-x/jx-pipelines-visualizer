4344e6b38167660a8eb34c827921917d381fac44
James Strachan <james.strachan@gmail.com>
2020-10-07 12:39:09 +0100

    fix: add helper method to work with v2 or v3 style labels

--- informer.go
+++ informer.go
@@ -31,7 +31,7 @@ func (i *Informer) Start(ctx context.Context) {
 			pa := obj.(*jenkinsv1.PipelineActivity)
 
 			// don't index (static) Jenkins PipelineActivity
-			if _, found := pa.Labels["context"]; !found {
+			if _, found := GetContext(pa); !found {
 				if i.Logger != nil && i.Logger.IsLevelEnabled(logrus.DebugLevel) {
 					i.Logger.WithField("PipelineActivity", pa.Name).Debug("Ignoring PipelineActivity without context")
 				}
@@ -51,7 +51,7 @@ func (i *Informer) Start(ctx context.Context) {
 			pa := new.(*jenkinsv1.PipelineActivity)
 
 			// don't index (static) Jenkins PipelineActivity
-			if _, found := pa.Labels["context"]; !found {
+			if _, found := GetContext(pa); !found {
 				if i.Logger != nil && i.Logger.IsLevelEnabled(logrus.DebugLevel) {
 					i.Logger.WithField("PipelineActivity", pa.Name).Debug("Ignoring PipelineActivity without context")
 				}
@@ -80,4 +80,18 @@ func (i *Informer) Start(ctx context.Context) {
 	})
 
 	informerFactory.Start(ctx.Done())
+}
+
+// GetContext returns the pipeline context using the v2 or v3 labels
+func GetContext(pa *jenkinsv1.PipelineActivity) (string, bool) {
+	if pa.Spec.Context != "" {
+		return pa.Spec.Context, true
+	}
+	for _, label := range []string{"context", "lighthouse.jenkins-x.io/context"} {
+		answer := pa.Labels[label]
+		if answer != "" {
+			return answer, true
+		}
+	}
+	return "", false
 }
